8e483aec774783959611612a3a57099cbc351bf7
VOVANDIY <97969526+VOVANDIY@users.noreply.github.com>
2022-02-23 21:09:56 +0200

    Update main.js
    Дз 2

--- js/main.js
+++ js/main.js
@@ -1,33 +1,24 @@
-const getCheck = (number) => number<0 ? -1 : number;
 
-getCheck(1);
-
-const getRandInt = (min,max) => Math.floor(Math.random()*(max-min+1)) + min;
-
-const getRantDir = (min,max) => min>max ? getRandInt(max,min) : getRandInt(min,max);
-
-const getIntRand = (num1,num2) =>  (getCheck(num1) && getCheck(num2)) === true ? getRantDir(num1 , num2) :-1;
-
-
-getIntRand(1,2);
-
-const getCoordinates = (num1,num2,numSig) => {
-  num1=num1*Math.pow(10, numSig);
-  num2=num2*Math.pow(10, numSig);
-  const rez = ( !(getCheck(num1) && getCheck(num2)) === -1) ? -1 : getRandInt(num1 , num2);
-  return rez/Math.pow(10, numSig);
+const getIntRandom = (num1,num2) => {
+  const getCheck = (number) => number<0 ? 0 : number;
+  const getRandomInt = (min,max) => Math.floor(Math.random()*(max-min+1)) + min;
+  const getRandomDir = (numA,numB) => numA>numB ? getRandomInt(numB,numA) : getRandomInt(numA,numB);
+  const rez=(getCheck(num1) && getCheck(num2)) === true ? getRandomDir(num1 , num2) :-1;
+  return rez;
 };
 
-getCoordinates(1,2,3);
+getIntRandom(1,2);
+
 //В работе использовались материалы  https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-const  getRandArbit = (min, max) =>  Math.random() * (max - min+1) + min;
+const getCoordinates = (strt,nd,numSig) => {
+  const getCheck = (number) => number<0 ? 0 : number;
+  const getRandomArbitrary = (min,max) => (Math.random()*(max-min+1)) + min;
+  const getRandomDir = (numA,numB) => numA>numB ? getRandomArbitrary(numB,numA) : getRandomArbitrary(numA,numB);
+  const rez=(getCheck(strt) && getCheck(nd) && getCheck(numSig) && numSig<21) === true ? getRandomDir(strt , nd) :-1;
+  return rez.toFixed(numSig);
+};
 
-const getRantArbitDir = (min,max) => min>max ? getRandArbit(max,min) : getRandArbit(min,max);
 
-const getRantArbitDirCheck = (num1,num2) =>  (getCheck(num1) && getCheck(num2)) === true ? getRantArbitDir(num1,num2) : -1;
-
-const getCoordinates2 = (strt,nd,numSig) => (getRantArbitDirCheck(strt,nd)*Math.pow(10, numSig)%1)/Math.pow(10, numSig);
-
-getCoordinates2(1,2,3);
+getCoordinates(1,2,3);
 //Получилось 2 варианта решения. И да самое сложное придумать название
